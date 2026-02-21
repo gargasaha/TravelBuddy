ba42fa24f9b39cae6698078e799cd0227217506f
{Garga Saha} <{sahagarga000@gmail.com}>
2026-02-22 01:44:57 +0530

    added community register functionility

--- Services/TravelDAL.cs
+++ Services/TravelDAL.cs
@@ -10,15 +10,47 @@ namespace TravelBuddy{
         {
             _configuration = configuration;
         }
-        // public async Task registerCommunity(Community community)
-        // {
-        //     conn=new SqlConnection(_configuration.GetConnectionString("db"));
-        //     await conn.OpenAsync();
-        //     cmd=new SqlCommand("insert into community values(@cname,@cpassword,@cimage)",conn);
-        //     cmd.Parameters.AddWithValue("@cname",community.cname);
-        //     cmd.Parameters.AddWithValue("@cpassword",community.cpassword);
-        //     cmd.Parameters.AddWithValue("@cimage",community.cimage);
-        //     await cmd.ExecuteNonQueryAsync();
-        // }
+        public async Task<int> registerCommunity(Community community,IFormFile ImageFile)
+        {
+            conn=new SqlConnection(_configuration.GetConnectionString("db"));
+            try{
+                await conn.OpenAsync();
+                cmd=new SqlCommand("registerCommunity",conn);
+                cmd.CommandType=System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@cname",community.cname);
+                cmd.Parameters.AddWithValue("@cpassword",community.cpassword);
+                byte[] imageBytes = new byte[0];
+                if (ImageFile != null && ImageFile.Length > 0)
+                {
+                    using var ms = new MemoryStream();
+                    await ImageFile.CopyToAsync(ms);
+                    imageBytes = ms.ToArray();
+                }
+                cmd.Parameters.AddWithValue("@cimage",imageBytes);
+                cmd.Parameters.AddWithValue("@cemail",community.cemail);
+                rd=await cmd.ExecuteReaderAsync();
+                if (rd.Read())
+                {
+                    if(rd.FieldCount > 0 && Convert.ToInt32(rd[0]) == -1)
+                    {
+                        return -1;
+                    }
+                    
+                }
+                else if (rd.Read() && Convert.ToInt32(rd[0]) == 1)
+                {
+                    return 1;
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Error registering community: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return 0;
+        }
     }
 }

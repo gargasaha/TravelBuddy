f83abd7b87af601529b971b521b5ce47df39a242
{Garga Saha} <{sahagarga000@gmail.com}>
2026-02-22 02:27:00 +0530

    added community register functionility

--- obj/Debug/net8.0/TravelBuddy.AssemblyInfo.cs
+++ obj/Debug/net8.0/TravelBuddy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelBuddy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba42fa24f9b39cae6698078e799cd0227217506f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40ebc62de1e2afa9f8673193a0b25392b763ce65")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelBuddy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelBuddy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net8.0/TravelBuddy.AssemblyInfo.cs
+++ obj/Release/net8.0/TravelBuddy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelBuddy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a3456012f1290ded8feef9f74c7511903c5f1c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40ebc62de1e2afa9f8673193a0b25392b763ce65")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelBuddy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelBuddy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

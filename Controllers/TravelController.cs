4a3456012f1290ded8feef9f74c7511903c5f1c0
{Garga Saha} <{sahagarga000@gmail.com}>
2026-02-22 01:38:02 +0530

    added community register functionility

--- Controllers/TravelController.cs
+++ Controllers/TravelController.cs
@@ -8,38 +8,12 @@ public class TravelController : Controller
 {
     private readonly ILogger<TravelController> _logger;
     private readonly IConfiguration _configuration;
-    private TravelDAL travelDAL;
+
     public TravelController(ILogger<TravelController> logger, IConfiguration configuration)
     {
         _logger = logger;
         _configuration = configuration;
-        travelDAL=new TravelDAL(_configuration);
     }
-    [HttpGet("/Travel/CreateCommunity")]
-    public IActionResult CreateCommunity()
-    {
-        ViewData["Message"] = null;
-        return View();
-    }
-    [HttpPost]
-    public async Task<IActionResult> CreateCommunity(Community community,IFormFile ImageFile)
-    {
-        // Console.WriteLine("Received community data: " + community.cname + ", " + community.cemail+ ", ImageFile: " + (ImageFile != null ? ImageFile.FileName : "No file")+community.cpassword);
-        var i=await travelDAL.registerCommunity(community,ImageFile);
-        if (i == -1)
-        {
-            ViewData["Message"]="Email already exists. Please use a different email.";
-            return View();
-        }
-        else if(i == 1)
-        {
-            ViewData["Message"]="Community created successfully!";
-            return View();
-        }
-        return RedirectToAction("Index");
-    }
-
-
 
     public IActionResult Index()
     {
--- obj/Debug/net8.0/TravelBuddy.AssemblyInfo.cs
+++ obj/Debug/net8.0/TravelBuddy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelBuddy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37d13ac2456c734504e517c3dda70551c28f958b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81f022f0c0c63efd5b6a523db4027b82a8173252")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelBuddy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelBuddy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
